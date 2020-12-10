3196ebc29faea8d631040415b148cb68be7ddcf6
linhaigoo <linhaigoo2013@gmail.com>
2020-12-10 08:31:37 +0800

    [feature] blink & menu

--- TASK/TASK_DISPLAY.c
+++ TASK/TASK_DISPLAY.c
@@ -4,18 +4,16 @@
 
 #include "encoder.h"
 #include "snprintfModify.h"
-/***********************************
-Íâ²¿ÊäÈëÇë¼ÓÔÚ¸ÃÈÎÎñÖÐ
-************************************/
 
-
-/******È«¾Ö×´Ì¬±äÁ¿ÉùÃ÷********/
-
-/***************************/
 
 #define ILIM_MAX 6.5f
 #define VLIM_MAX 42.0f
 #define ID_MAX   8
+#define MODE_NUM 2
+#define LOST_FOCUS_TIME 200
+#define OVER_LAP_VAR_TIME 20
+#define VOL_MODE 0
+#define AMP_MODE 1
 
 char SCREEN_BUFFER[6][19] = 
 {
@@ -35,6 +33,7 @@ typedef enum
 	UINT8_T,
 	FLOAT_T,
 	STRING,
+	STRING_VAR,
 	WAVE,
 }VAR_TypeEdef;
 
@@ -44,7 +43,7 @@ typedef struct
 	uint8_t pos_y;
 	uint8_t spaceLen;
 	VAR_TypeEdef type;
-	uint8_t name_id;
+	uint8_t promp_name_id;
 	uint8_t rvar_id; //relative var id
 	uint8_t ifOverLap;
 	void * ptr;
@@ -54,9 +53,11 @@ typedef enum
 {
 	VAR_EMPTY = 0,
 	CURRENT_VAR = 1,
+	CURRENT_VAR_PROMP = 2,
 	ALL_T_DISPALY = 2,
 	ON_T_DISPALY = 3,
 	VOL_VAR = 4,
+	VOL_VAR_PROMP = 6,
 	WAVE_DIAPLAY = 5,
 	
 	MODE_PROMP = 6,
@@ -72,6 +73,8 @@ typedef enum
 	ID_VAR = 15,
 	I_LIM_VAR = 16,
 	V_LIM_VAR = 17,
+	CURRENT_SET_VAR = 18,
+	VOL_SET_VAR = 19,
 }SCREEN_VAR_ID_Typedef;
 
 
@@ -87,7 +90,7 @@ typedef struct
 	float on_t;
 	float all_t;
 	float time;
-	uint8_t curMODE;
+	uint8_t curMODE;  0 :vol 1:amp
 	uint8_t ID;
 	uint8_t isWorking;
 }PM_Typedef;
@@ -98,35 +101,58 @@ SCREEN_VAR_Typedef screen_vars[]=
 {
 	{0,0,0,0,0,0,NULL},
 	//id 1-5
-	{0,0,3,FLOAT_T,0,0,1, &PM_module.iCur},    //current dispaly 
-	{5,0,3,FLOAT_T,0,0,0, &PM_module.all_t},   //ALL_T dispaly
-	{11,0,3,FLOAT_T,0,0,0, &PM_module.on_t}, //ON_T dispaly
-	{0,1,3,FLOAT_T,0,0,1, &PM_module.vCur}, //voltage dispaly
+	{0,0,3,FLOAT_T,CURRENT_VAR_PROMP,0,1, &PM_module.iCur},    //current dispaly
+	{3,0,3,STRING,0,0,1, "A"},    //current dispaly
+	{5,0,3,FLOAT_T,ALL_T_PROMP,0,0, &PM_module.all_t},   //ALL_T dispaly
+	{11,0,3,FLOAT_T,ON_T_PROMP,0,0, &PM_module.on_t}, //ON_T dispaly
+	{0,1,3,FLOAT_T,VOL_VAR_PROMP,0,1, &PM_module.vCur}, //voltage dispaly
+	{3,1,3,STRING,0,0,1, "V"},              //voltage dispaly
 	{8,1,8,WAVE,0,0,1, &PM_module.time},   //wave dispaly	
 	
 	//id 6-11
-	{0,2,4,STRING,0,0,0,NULL},   	//MODE string
-	{6,2,4,STRING,0,0,0,NULL},     //AL_T string
-	{12,2,4,STRING,0,0,0,NULL}, 	 //ON_T string	
-	{0,3,4,STRING,0,0,0,&PM_module.curMODE},   	//VOL/AMP string
-	{6,3,4,FLOAT_T,0,ALL_T_DISPALY,0,&PM_module.all_t},     //AL_T var
-	{12,3,4,FLOAT_T,0,ON_T_DISPALY,0,&PM_module.on_t}, 	 //ON_T var	
+	{0,2,4,STRING,0,0,0,"MODE"},   	//MODE string
+	{6,2,4,STRING,0,0,0,"AL_T"},     //AL_T string
+	{12,2,4,STRING,0,0,0,"ON_T"}, 	 //ON_T string	
+	{0,3,4,STRING_VAR, MODE_PROMP,0,0,&PM_module.curMODE},   	//VOL/AMP string
+	{6,3,4,FLOAT_T, ALL_T_PROMP,0,0,&PM_module.all_t},     //AL_T var
+	{12,3,4,FLOAT_T,ON_T_PROMP,0,0,&PM_module.on_t}, 	 //ON_T var	
 	
 	//id 12-17
-	{0,4,4,STRING,0,0,0,NULL},   	//ID string
-	{6,4,4,STRING,0,0,0,NULL},     //I_LIM string
-	{12,4,4,STRING,0,0,0,NULL}, 	  //V_LIM string	
-	{0,5,3,UINT8_T,0,0,0,&PM_module.ID},   	//ID var
-	{6,5,4,FLOAT_T,0,0,0,&PM_module.I_LIM},    //I_LIM var
-	{12,5,4,FLOAT_T,0,0,0,&PM_module.V_LIM}, 	//V_LIM var	
+	{0,4,4,STRING,0,0,0,"ID"},   	//ID string
+	{6,4,4,STRING,0,0,0,"ILIM"},     //I_LIM string
+	{12,4,4,STRING,0,0,0,"VLIM"}, 	  //V_LIM string	
+	{0,5,3,UINT8_T,ID_PROMP,0,0,&PM_module.ID},   	//ID var
+	{6,5,4,FLOAT_T,I_LIM_PROMP,0,0,&PM_module.I_LIM},    //I_LIM var
+	{12,5,4,FLOAT_T,V_LIM_PROMP,0,0,&PM_module.V_LIM}, 	//V_LIM var	
 };
+
+char screen_string_vars[][5]=
+{
+	{"A"},
+	{"V"},
+	{"VOL"},
+	{"AMP"},
+}
+
+const SCREEN_VAR_ID_Typedef screen_menu[]=
+{
+	CURRENT_VAR, VOL_VAR, MODE_VAR, ALL_T_VAR, ON_T_VAR, ID_VAR, I_LIM_VAR, V_LIM_VAR,
+}
+const uint8_t MENU_SIZE = sizeof(screen_menu) / sizeof(SCREEN_VAR_ID_Typedef);
+
+#define MODE_STRING_INDEX_START 2
+#define MODE_STRING_INDEX_STOP 3
 
 typedef struct
 {
 	uint8_t selectedVarID;
+	uint8_t menuID;
+	uint8_t prompID;
 	uint8_t isModifying;
 	uint16_t unOperationDuration;
 	uint8_t curPage;
+	const char * str;
+	uint8_t str_InvMode;
 }INTERACT_MANGE_Typedef;
 
 
@@ -139,9 +165,24 @@ SCREEN_VAR_ID_Typedef varValueParser(SCREEN_VAR_ID_Typedef varID, PM_Typedef * p
 {
 	float float_tmp;
 	uint8_t uint8_t_tmp;
+	uint8_t string_id_tmp;
 	SCREEN_VAR_ID_Typedef varUpdateID = VAR_EMPTY;
 	switch(type)
 	{
+		case STRING_VAR:
+			string_id_tmp = *(uint8_t*) pVarTmp;
+			switch(varID)
+			{
+				case MODE_VAR:
+					string_id_tmp = string_id_tmp % (MODE_STRING_INDEX_STOP + 1 - MODE_STRING_INDEX_START)
+									 + MODE_STRING_INDEX_START;
+					break;
+				default:
+					
+					break;
+			}
+			*(uint8_t*)pVarTmp = string_id_tmp;
+			break;
 		case UINT8_T:
 			uint8_t_tmp = *(uint8_t*) pVarTmp;
 			switch(varID)
@@ -162,10 +203,22 @@ SCREEN_VAR_ID_Typedef varValueParser(SCREEN_VAR_ID_Typedef varID, PM_Typedef * p
 				case CURRENT_VAR:
 					float_tmp = (float_tmp > 0) ? float_tmp : 0;
 					float_tmp = (float_tmp < pPM->I_LIM) ? float_tmp : 0;
+					if(pPM->curMODE == VOL_MODE)
+					{
+						pPM->isWorking = false;
+						pPM->curMODE = AMP_MODE;
+						varUpdateID = MODE_VAR;		
+					}
 					break;
 				case VOL_VAR:
 					float_tmp = (float_tmp > 0) ? float_tmp : 0;
 					float_tmp = (float_tmp < pPM->V_LIM) ? float_tmp : 0;
+					if(pPM->curMODE == AMP_MODE)
+					{
+						pPM->isWorking = false;
+						pPM->curMODE = VOL_MODE;
+						varUpdateID = MODE_VAR;
+					}
 					break;
 				case ALL_T_VAR:
 					float_tmp = (float_tmp > 0) ? float_tmp : 0;
@@ -209,7 +262,7 @@ SCREEN_VAR_ID_Typedef varValueParser(SCREEN_VAR_ID_Typedef varID, PM_Typedef * p
 
 const char * strPrepare(SCREEN_VAR_ID_Typedef id)
 {
-	int strLen;
+	int strLen = 0;
 	switch(screen_vars[id].type)
 	{
 		case UINT8_T:
@@ -218,18 +271,40 @@ const char * strPrepare(SCREEN_VAR_ID_Typedef id)
 		case FLOAT_T:
 			strLen = MySnprintfFloat(*(float *)screen_vars[id].ptr, &snprintfBuff[BLANK_SPACE_PRESERVE], screen_vars[id].spaceLen+1, 1);
 			break;
+		case STRING_VAR:
+		case STRING:
+		    const char * strTmp = screen_vars[id].type == STRING ? (char*)screen_vars[id].ptr : &screen_string_vars[screen_vars[id].promp_name_id][0];
+			char * strBuff = &snprintfBuff[BLANK_SPACE_PRESERVE];
+			while(strTmp != '\0' && strLen < 10)
+			{
+				*(strBuff++) = *(strTmp++);
+				strLen++;
+			}
+			*strBuff = '\0';
 		default:
 			break;
 	}
+
 	if(strLen > 0)
 	{
 		int spaceFill = screen_vars[id].spaceLen - strLen;
-		if(spaceFill > 0 && spaceFill < BLANK_SPACE_PRESERVE)
+		if(spaceFill >= 0 && spaceFill < BLANK_SPACE_PRESERVE)
 		{
 			return &snprintfBuff[BLANK_SPACE_PRESERVE-spaceFill];
 		}
 	}
 	return NULL;
+}
+
+bool strDirectShow(SCREEN_VAR_ID_Typedef id)
+{
+	const char * str = strPrepare(id);
+	if(str != NULL)
+	{
+		OLED_ShowString(screen_vars[id].pos_x, (screen_vars[id].pos_y % 2) << 1, str, false);
+		return true;
+	}
+	return false;
 }
 
 
@@ -240,22 +315,29 @@ bool strFill(INTERACT_MANGE_Typedef * pIntrMange, SCREEN_VAR_ID_Typedef id, bool
 		const char * str = strPrepare(id);
 		if(str != NULL)
 		{
-			if()
-			OLED_ShowString(screen_vars[id].pos_x, (screen_vars[id].pos_y % 2) << 1, str, inv_mode);
+			if(screen_vars[id].pos_y >>2 == pIntrMange->curPage)
+			{
+				pIntrMange->str = str;
+				pIntrMange->str_InvMode = inv_mode;
+				OLED_ShowString(screen_vars[id].pos_x, (screen_vars[id].pos_y % 2) << 1, str, inv_mode);
+			}
+			
 			if(sync)
 			{
-				char * pSCREEN_BUFFER_start = &SCREEN_BUFFER[screen_vars[rvar_id].pos_y][screen_vars[rvar_id].pos_x];
-				while(*(str++)!='\0')
+				const char * strTmp = str;
+				char * pSCREEN_BUFFER_start = &SCREEN_BUFFER[screen_vars[id].pos_y][screen_vars[id].pos_x];
+				while(*strTmp!='\0')
 				{
-					*(pSCREEN_BUFFER_start++) = *(str++);
+					*(pSCREEN_BUFFER_start++) = *(strTmp++);
 				}
 				u8 rvar_id = screen_vars[id].rvar_id;
 				if(rvar_id != VAR_EMPTY)
 				{
+					const char * strTmp = str;
 					char * pSCREEN_BUFFER_start = &SCREEN_BUFFER[screen_vars[rvar_id].pos_y][screen_vars[rvar_id].pos_x];
-					while(*(str++)!='\0')
+					while(*strTmp!='\0')
 					{
-						*(pSCREEN_BUFFER_start++) = *(str++);
+						*(pSCREEN_BUFFER_start++) = *(strTmp++);
 					}
 				}
 			}
@@ -268,22 +350,38 @@ bool strFill(INTERACT_MANGE_Typedef * pIntrMange, SCREEN_VAR_ID_Typedef id, bool
 
 void interact(INTERACT_MANGE_Typedef * pIntrMange, PM_Typedef * pPM, u8 key_pressed, s16 count)
 {
+	if(key_pressed ï½œ count != 0)
+	{
+		pIntrMange->unOperationDuration = 0;
+	}
+	else
+	{
+		pIntrMange->unOperationDuration++;
+	}
+	
+	
 	if(key_pressed)
 	{
-		if(pIntrMange->isModifying == VAR_EMPTY)
+		key_pressed = false;
+		if(pIntrMange->selectedVarID == VAR_EMPTY)
 		{
-			pPM->isWorking = !pPM->isWorking;
+			pPM->isWorking = false;
 		}
 		else
 		{
 			pIntrMange->isModifying = !pIntrMange->isModifying;
 			if(pIntrMange->isModifying)
-			{		
+			{
+				//menu promp >-->  value 
+				OLED_ShowString(screen_vars[pIntrMange->prompID].pos_x, (screen_vars[IntrMange->prompID].pos_y % 2) << 1, pIntrMange->str, false);
 				strFill(pIntrMange->selectedVarID, false, true);
 			}
 			else
 			{
+
 				strFill(pIntrMange->selectedVarID, true, false);
+				//menu promp <--<  value
+				strFill(pIntrMange->prompID, false, true);
 			}
 		}
 	}
@@ -299,28 +397,129 @@ void interact(INTERACT_MANGE_Typedef * pIntrMange, PM_Typedef * pPM, u8 key_pres
 				case UINT8_T:
 					int_tmp = *(uint8_t*)screen_vars[pIntrMange->selectedVarID].ptr + (count>>2);
 					SCREEN_VAR_ID_Typedef updateID = varValueParser(pIntrMange->selectedVarID, pPM, &float_tmp, FLOAT_T);
-					*(uint8_t*)screen_vars[pIntrMange->selectedVarID].ptr = float_tmp;
+					*(uint8_t*)screen_vars[pIntrMange->selectedVarID].ptr = int_tmp;
 				break;
 					
 				case FLOAT_T:
 					
 				float_tmp = *(float*)screen_vars[pIntrMange->selectedVarID].ptr + 0.01 * count;
 				SCREEN_VAR_ID_Typedef updateID = varValueParser(pIntrMange->selectedVarID, pPM, &float_tmp, FLOAT_T);
-				if()
+				
 				*(float*)screen_vars[pIntrMange->selectedVarID].ptr = float_tmp;
-				strFill(updateID, false, true);
-				strFill(pIntrMange->selectedVarID, true);
 
-				case STRING:
+				case STRING_VAR:
+
+				u8 name_id = *(float*)screen_vars[pIntrMange->selectedVarID].promp_name_id + count;
+				SCREEN_VAR_ID_Typedef updateID = varValueParser(pIntrMange->selectedVarID, pPM, &name_id, FLOAT_T);
+				
+				*(char*)screen_vars[pIntrMange->selectedVarID].ptr = &screen_string_vars[name_id][0];
+
 					break;
 				case WAVE:
 					break;
 			}
-			if(screen_vars[pIntrMange->selectedVarID].rvar_id != 0)
+
+			if(updateID != VAR_EMPTY)
 			{
-				strFill(pIntrMange, screen_vars[pIntrMange->selectedVarID].rvar_id, strLen, true);
-			}			
+				strFill(updateID, true, false);
+			}
+			strFill(pIntrMange->selectedVarID, false, true);
 		}
+
+		else
+		{
+			pIntrMange->menuID = (pIntrMange->menuID + count>>2) % MENU_SIZE;
+			pIntrMange->selectedVarID = screen_menu[pIntrMange->menuID];
+			
+			if(screen_vars[newSelectID].pos_y >>1 == pIntrMange->curPage)
+			{
+				OLED_ShowString(screen_vars[pIntrMange->prompID].pos_x, (screen_vars[IntrMange->prompID].pos_y % 2) << 1, pIntrMange->str, false);
+			}
+			else
+			{
+				//page switch
+				pIntrMange->curPage = screen_vars[newSelectID].pos_y >>1;
+				OLED_ShowString(0, 0, SCREEN_BUFFER[pIntrMange->curPage<<1], false);
+				OLED_ShowString(0, 2, SCREEN_BUFFER[pIntrMange->curPage<<1+1], false);
+			}
+			
+			pIntrMange->prompID = screen_vars[pIntrMange->selectedVarID].promp_name_id;
+			strFill(pIntrMange->prompID, false, true);
+		}
+	}
+	else
+	{
+		if(pIntrMange->unOperationDuration > LOST_FOCUS_TIME)
+		{
+			pIntrMange->unOperationDuration = LOST_FOCUS_TIME;
+			if(pIntrMange->selectedVarID != VAR_EMPTY)
+			{
+				pIntrMange->selectedVarID = VAR_EMPTY;
+
+				//lost focus display
+				if(pIntrMange->isModifying)
+				{
+					pIntrMange->isModifying =false;
+					strFill(pIntrMange->selectedVarID, false, false);
+				}
+				else
+				{
+					OLED_ShowString(screen_vars[pIntrMange->prompID].pos_x, (screen_vars[IntrMange->prompID].pos_y % 2) << 1, pIntrMange->str, false);
+				}
+			}
+		}
+		else
+		{
+			//blink
+			pIntrMange->str_InvMode = !pIntrMange->str_InvMode;
+
+			uint8_t id = pIntrMange->isModifying ? pIntrMange->selectedVarID : pIntrMange->prompID;
+
+			OLED_ShowString(screen_vars[id].pos_x, (screen_vars[id].pos_y % 2) << 1, pIntrMange->str, pIntrMange->str_InvMode);
+		}
+		
+	}
+}
+
+
+void showRealTimeVar(INTERACT_MANGE_Typedef * pIntrMange, PM_Typedef * pPM, u8 key_pressed, s16 count)
+{
+	uint8_t realTimeVarID;
+	if(pIntrMange->curPage == 0)
+	{
+		if(pPM->isWorking)
+		{
+			if(pIntrMange->isModifying && screen_vars[pIntrMange->selectedVarID].ifOverLap)
+			{
+				if(pIntrMange->unOperationDuration > OVER_LAP_VAR_TIME)
+				{
+					//exit var fune state
+					pIntrMange->isModifying = false;
+					strFill(pIntrMange->prompID, false, true);
+				}
+			}
+
+			if(!(pIntrMange->isModifying && pIntrMange->selectedVarID == CURRENT_VAR))
+			{
+				strDirectShow(CURRENT_VAR);
+			}
+			if(!(pIntrMange->isModifying && pIntrMange->selectedVarID == VOL_VAR))
+			{
+				strDirectShow(VOL_VAR);
+			}
+
+
+			OLED_ShowString(screen_vars[WAVE_DIAPLAY].pos_x, (screen_vars[WAVE_DIAPLAY].pos_y % 2) << 1, "___OFF__")
+		}
+		else
+		{
+				strDirectShow(CURRENT_SET_VAR);
+				strDirectShow(VOL_SET_VAR);
+
+				//specific
+				OLED_ShowString(screen_vars[WAVE_DIAPLAY].pos_x, (screen_vars[WAVE_DIAPLAY].pos_y % 2) << 1, "___OFF__");
+		}
+		
 	}
 }
 
@@ -348,7 +547,7 @@ void TASK_DISPLAY(void *pdata)
 		MySnprintfInt(encPos, Buff, 20);
 		
 		/************************/
-		//ÊµÊ±ÏÔÊ¾
+
 		OLED_ShowString(0,0, SCREEN_BUFFER[count%6],0);
 		OLED_ShowString(0,2, SCREEN_BUFFER[count%6+1],0);
 		count+=2;
